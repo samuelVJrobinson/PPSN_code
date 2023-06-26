30034367fea107b876458637a002864ecd3f80d6
samuelVJrobinson <samuel.vj.robinson@gmail.com>
2023-06-26 14:09:13 -0600

    Updated grower report
    - Couldn't get officer to place tables and figures in correct places

--- yieldProfitEst.R
+++ yieldProfitEst.R
@@ -10,6 +10,7 @@ library(tidyverse)
 library(scales)
 library(stars)
 library(ggpubr)
+library(officer)
 
 # setwd("D:/geoData/SMSexport")
 
@@ -162,8 +163,6 @@ canProf %>% filter(CropType %in% top6crop) %>%
 
 # Report for growers ------------------------------------------------------
 
-library(officer)
-
 temp <- canProf[[1]] #Single grower/year
 
 #Table of summary yields
@@ -176,8 +175,8 @@ yieldTable <- temp %>% separate(FieldYear,c('Field','Year'),sep='_') %>%
   mutate(medYield=ifelse(is.na(medYield),'-',as.character(medYield))) %>%
   ungroup() %>% pivot_wider(names_from='CropType',values_from='medYield') %>%
   arrange(desc(Year)) %>%   
-  slice(1:5) %>% column_to_rownames('Year') %>% 
-  select(1:6)
+  slice(1:5) %>% #column_to_rownames('Year') %>% 
+  select(1:7)
 
 #Figure of profit distributions
 profitFig <- temp %>% filter(!is.na(Profit_ac)) %>% group_by(CropType) %>%
@@ -199,18 +198,88 @@ percMargAc <- temp %>% filter(!is.na(Profit_ac)) %>%
   pull(NegProf) %>% mean(.)*100
 percMargAc <- ifelse(percMargAc<1,'less than 1',as.character(round(percMargAc)))
 
-template <- read_docx('./newsletter2023/templatePg2.docx') %>% 
+read_docx('./newsletter2023/fullTemplate.docx') %>% 
   body_replace_all_text(old_value = 'GROWERNAME',new_value = 'Clinton') %>% 
   body_replace_all_text(old_value = 'NUMFIELDYEARS',new_value = as.character(numFieldYears)) %>% 
   body_replace_all_text(old_value = 'NUMCROPS',new_value = as.character(numCropTypes)) %>% 
   body_replace_all_text(old_value = 'NUMYEARS',new_value = as.character(numYears)) %>% 
   body_replace_all_text(old_value = 'PERCMARGACRES',new_value = as.character(percMargAc)) %>% 
-  # cursor_reach(keyword='FIGUREHERE') %>% 
-  body_replace_all_text(old_value = 'FIGUREHERE',new_value = '') %>% 
-  body_add_gg(value=profitFig,width = 7.5,height=4.5)
+  #Add table 
+  body_add_table(value=yieldTable,style = 'Normal Table' ) %>%
+  body_add_par(value = '') %>% 
+  # Add figure
+  body_add_gg(value=profitFig,width = 7.5,height=4,scale = 1.2) %>%
+  print(target = './newsletter2023/templateTest.docx')
 
-print(template,target = './newsletter2023/templatePg2_test.docx')
-# docx_update(input = './newsletter2023/templatePg2_test.docx')
+##All growers
+
+#Load list of growers
+growerDat <- read.csv('./data/growerCSV.csv',strip.white = TRUE) %>% 
+  rename_with(~gsub('.','',.x,fixed = TRUE)) %>% 
+  select(GrowerID:BusinessName) %>% 
+  #Amalgamate first and last name if businessname is blank
+  mutate(BusinessName=ifelse(is.na(BusinessName)|BusinessName=='',paste(FirstName,LastName),BusinessName)) %>% 
+  mutate(BusinessName=gsub('\\.$','',BusinessName)) #Remove trailing periods
+
+for(i in 1:3){
+  gID <- sapply(str_split(names(canProf)[i],' '),first) %>% gsub('-.*','',.)
+  gName <- growerDat$FirstName[which(growerDat$GrowerID==gID)]
+  fName <- growerDat$BusinessName[which(growerDat$GrowerID==gID)]
+  
+  temp <- canProf[[i]] #Data for grower
+  
+  #Table of summary yields
+  yieldTable <- temp %>% separate(FieldYear,c('Field','Year'),sep='_') %>% 
+    group_by(CropType) %>% mutate(N=n()) %>% arrange(desc(N)) %>% 
+    ungroup() %>% 
+    mutate(CropType=factor(CropType,levels=unique(CropType))) %>% 
+    group_by(Year,CropType,.drop = FALSE) %>% 
+    summarize(medYield=round(median(Yield_buAc,na.rm=TRUE),0),.groups = 'keep') %>% 
+    mutate(medYield=ifelse(is.na(medYield),'-',as.character(medYield))) %>%
+    ungroup() %>% pivot_wider(names_from='CropType',values_from='medYield') %>%
+    arrange(desc(Year)) %>%   
+    slice(1:pmin(5,nrow(.))) %>% #column_to_rownames('Year') %>% 
+    select(1:pmin(7,ncol(.)))
+  
+  #Figure of profit distributions
+  profitFig <- temp %>% filter(!is.na(Profit_ac)) %>% 
+    filter(Profit_ac<quantile(Profit_ac,0.99)) %>% #Removes top 1% yield outliers 
+    group_by(CropType) %>%
+    mutate(CropType=paste0(toupper(CropType),', Yield: ',round(median(Yield_buAc)),' bu/ac\nProfit: $',round(median(Profit_ac)),'/ac, Marginal acres: ',round(100*mean(Profit_ac<0),1),'%')) %>% 
+    ggplot(aes(x=Profit_ac))+
+    geom_density(fill='grey90')+
+    facet_wrap(~CropType)+
+    geom_vline(xintercept = 0,col='red')+
+    scale_y_continuous(labels = percent, name = "Percent of acres") +
+    labs(x='Profit ($/ac)',title=paste0('Farm: ',fName,', Year: 2022'))+theme_bw()#+
+  
+  #Other values to replace
+  numFieldYears <- length(unique(temp$FieldYear))
+  numCropTypes <- length(unique(temp$CropType))
+  numYears <- temp %>% separate(FieldYear,c('Field','Year'),sep='_') %>% 
+    select(Year) %>% distinct() %>% nrow()
+  percMargAc <- temp %>% filter(!is.na(Profit_ac)) %>% 
+    mutate(NegProf=Profit_ac<0) %>% 
+    pull(NegProf) %>% mean(.)*100
+  percMargAc <- ifelse(percMargAc<1,'less than 1',as.character(round(percMargAc)))
+  
+  read_docx('./newsletter2023/fullTemplate.docx') %>% 
+    body_replace_all_text(old_value = 'GROWERNAME',new_value = gName) %>% 
+    body_replace_all_text(old_value = 'NUMFIELDYEARS',new_value = as.character(numFieldYears)) %>% 
+    body_replace_all_text(old_value = 'NUMCROPS',new_value = as.character(numCropTypes)) %>% 
+    body_replace_all_text(old_value = 'NUMYEARS',new_value = as.character(numYears)) %>% 
+    body_replace_all_text(old_value = 'PERCMARGACRES',new_value = as.character(percMargAc)) %>% 
+    #Add table 
+    body_add_table(value=yieldTable,
+                   # style = 'Table Grid',
+                   stylenames = prop_table(style='Table Grid'),
+                   alignment = c('r',rep('l',ncol(yieldTable)-1))) %>%
+    body_add_par(value = '') %>% 
+    # table_width(width = 1, unit = "pct") %>% 
+    # Add figure
+    body_add_gg(value=profitFig,width = 7.5,height=4,scale = 1) %>%
+    print(target = paste0('./newsletter2023/reports/',gID,' ',fName,' 2023 report.docx'))
+}
 
 
 # Other --------------------------
